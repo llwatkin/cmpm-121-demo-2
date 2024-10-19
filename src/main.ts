8ad9eb655ddecb1e88015cb4e8053b802c23c9b7
Lyle Watkins <llwatkin@ucsc.edu>
2024-10-18 17:46:16 -0700

    Step 2 complete

--- src/main.ts
+++ src/main.ts
@@ -10,4 +10,39 @@ titleDisplay.innerHTML = APP_NAME;
 app.append(titleDisplay);
 
 const canvas = document.createElement("canvas");
+canvas.height = 256;
+canvas.width = 256;
 app.append(canvas);
+
+const ctx = canvas.getContext("2d");
+
+const cursor = { active: false, x: 0, y: 0 };
+
+canvas.addEventListener("mousedown", (e) => {
+  cursor.active = true;
+  cursor.x = e.offsetX;
+  cursor.y = e.offsetY;
+});
+
+canvas.addEventListener("mousemove", (e) => {
+  if (cursor.active) {
+    ctx!.beginPath();
+    ctx!.moveTo(cursor.x, cursor.y);
+    ctx!.lineTo(e.offsetX, e.offsetY);
+    ctx!.stroke();
+    cursor.x = e.offsetX;
+    cursor.y = e.offsetY;
+  }
+});
+
+canvas.addEventListener("mouseup", () => {
+  cursor.active = false;
+});
+
+const clearButton = document.createElement("button");
+clearButton.innerHTML = "Clear";
+app.append(clearButton);
+
+clearButton.addEventListener("click", () => {
+  ctx!.clearRect(0, 0, canvas.width, canvas.height);
+});
