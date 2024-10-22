dad67b8efd679ce72456ccfc47773c3188d59995
Lyle Watkins <llwatkin@ucsc.edu>
2024-10-22 12:28:32 -0700

    Step 5 complete

--- src/main.ts
+++ src/main.ts
@@ -21,42 +21,62 @@ interface Point {
   x: number;
   y: number;
 }
+
 interface Line {
   points: Array<Point>;
+
+  drag(newPoint: Point): void;
+  display(ctx: CanvasRenderingContext2D): void;
 }
-const currLine: Line = { points: [] };
+
+function createLine(initPoint?: Point): Line {
+  const points: Array<Point> = [];
+  if (initPoint) {
+    points.push(initPoint);
+  }
+
+  return {
+    points: points,
+    drag: (newPoint: Point) => {
+      points.push(newPoint);
+    },
+    display: (ctx: CanvasRenderingContext2D) => {
+      for (let i = 0; i < points.length - 1; i++) {
+        ctx!.beginPath();
+        ctx!.moveTo(points[i].x, points[i].y);
+        ctx!.lineTo(points[i + 1].x, points[i + 1].y);
+        ctx!.stroke();
+      }
+    },
+  };
+}
+
+let currLine: Line;
 let allLines: Array<Line> = [];
 let redoLines: Array<Line> = [];
 
 const drawingChangeEvent = new Event("drawing-changed");
 
-canvas.addEventListener("mousedown", () => {
+canvas.addEventListener("mousedown", (e) => {
   cursor.active = true;
+  const initPoint: Point = { x: e.offsetX, y: e.offsetY };
+  currLine = createLine(initPoint);
 });
 
 canvas.addEventListener("mousemove", (e) => {
   if (cursor.active) {
     const newPoint: Point = { x: e.offsetX, y: e.offsetY };
-    currLine.points.push(newPoint);
+    currLine.drag(newPoint);
     canvas.dispatchEvent(drawingChangeEvent);
   }
 });
 
 canvas.addEventListener("mouseup", () => {
   cursor.active = false;
-  const finishedLine: Line = { points: currLine.points };
-  allLines.push(finishedLine); // Push finished line to the lines array
-  currLine.points = []; // Clear current points to start a new line
+  allLines.push(currLine); // Push finished line to the total lines array
+  currLine = createLine(); // Reset line to prepare for next stroke
+  redoLines = []; // Clear previous redo lines when a new line is drawn
 });
-
-function drawLine(line: Line) {
-  for (let i = 0; i < line.points.length - 1; i++) {
-    ctx!.beginPath();
-    ctx!.moveTo(line.points[i].x, line.points[i].y);
-    ctx!.lineTo(line.points[i + 1].x, line.points[i + 1].y);
-    ctx!.stroke();
-  }
-}
 
 function clearCanvas() {
   ctx!.clearRect(0, 0, canvas.width, canvas.height);
@@ -65,10 +85,10 @@ function clearCanvas() {
 canvas.addEventListener("drawing-changed", () => {
   clearCanvas();
   // Draw current line
-  drawLine(currLine);
+  currLine.display(ctx!);
   // Draw all past lines
   for (let i = 0; i < allLines.length; i++) {
-    drawLine(allLines[i]);
+    allLines[i].display(ctx!);
   }
 });
 
